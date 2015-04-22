4e5abf6df02b5e2808b999f62b20874bf9e64d3a
Fabio Rehm <fgrehm@gmail.com>
2015-04-22 16:46:31 -0300

    Done with producers and consumers app

--- producers-n-consumers.c
+++ producers-n-consumers.c
@@ -10,76 +10,77 @@ Mateus Vendramini
 #include <semaphore.h>
 #include "mymutex.h"
 
-#define NUM_THREADS 5
 #define PRODUCERS  3
-#define CONSUMERS  3
+#define CONSUMERS  7
+#define NUM_THREADS PRODUCERS+CONSUMERS
+#define BUFFER_SIZE 5
 
-//sem_t mutex, empty, full;
-my_mutex mutex, empty, full;
-int in=0, out=0, buffer[NUM_THREADS];
+sem_t empty, full;
+my_mutex mutex;
+
+int in=0, out=0, buffer[BUFFER_SIZE];
+
+void do_work(char* process_type, int thread_id, char* work) {
+  printf("[%s-%d] %s\n", process_type, thread_id, work);
+  int r = random() % 2;
+  sleep(r);
+}
 
 void *producer(void *arg){
   int item, tid;
-
   tid = (int)(long int)arg;
+
   while(1){
-    item=random();
+    do_work("PRODUCER", tid, "Generating item");
+    item = random() % 100;
 
-    //sem_wait(&empty);      /* wait() ou p() ou down() */
-    m_lock(&empty, tid);
+    sem_wait(&empty);
 
-    //sem_wait(&mutex);      /* wait() ou p() ou down() */
-    m_lock(&mutex, 1);
-
+    m_lock(&mutex, tid);
+    printf("[PRODUCER-%d] Writing '%d' at position '%d'\n", tid, item, in);
     buffer[in]=item;
-    printf("produtor %d escrevendo na pos %d\n", tid, in);
-    in=(in+1)%NUM_THREADS;
+    // Move in "pointer" to the next available slot
+    in=(in+1)%BUFFER_SIZE;
+    m_unlock(&mutex, tid);
 
-    //sem_post(&mutex);      /* post() ou v() ou up() ou signal() */
-    m_unlock(&mutex, 1);
-
-    //sem_post(&full);      /* post() ou v() ou up() ou signal() */
-    m_unlock(&full, tid);
-
+    sem_post(&full);
   }
 }
 
 void *consumer(void *arg){
-  int item, tid;
+  int item, tid, old_out;
 
   tid = (int)(long int)arg;
+  // This is needed since we spawn PRODUCERS threads before creating
+  // reader threads
+  tid += PRODUCERS;
+
   while(1){
+    sem_wait(&full);
 
-    //sem_wait(&full);      /* wait() ou p() ou down() */
-    m_lock(&full, tid);
-
-    //sem_wait(&mutex);      /* wait() ou p() ou down() */
-    m_lock(&mutex, 1);
-
+    m_lock(&mutex, tid);
+    old_out = out;
     item=buffer[out];
-    printf("consumidor %d retirando da pos %d\n", tid, out);
-    out=(out+1)%NUM_THREADS;
+    out=(out+1)%BUFFER_SIZE;
+    m_unlock(&mutex, tid);
 
-    //sem_post(&mutex);      /* post() ou v() ou up() ou signal() */
-    m_unlock(&mutex, 1);
+    char msg[50];
+    sprintf(msg, "Consuming item from position '%d', value '%d'", old_out, item);
+    do_work("CONSUMER", tid, msg);
 
-    //sem_post(&empty);      /* post() ou v() ou up() ou signal() */
-    m_unlock(&empty, tid);
-
-    item=0;
+    sem_post(&empty);
   }
 }
 
 int main(int argc, char *argv[]){
+  srandom(time(NULL));
+
   long int i;
   pthread_t producers[PRODUCERS], consumers[CONSUMERS];
 
-  //sem_init(&mutex,0,1);
-  m_init(&mutex, 1);
-  //sem_init(&empty,0,NUM_THREADS);
-  m_init(&empty, NUM_THREADS);
-  //sem_init(&full,0,0);
-  m_init(&full, NUM_THREADS);
+  m_init(&mutex, NUM_THREADS);
+  sem_init(&empty, 0, BUFFER_SIZE);
+  sem_init(&full, 0, 0);
 
   for(i = 0; i < PRODUCERS; i++)
     pthread_create(&producers[i], NULL, producer, (void *)i);
@@ -87,8 +88,8 @@ int main(int argc, char *argv[]){
     pthread_create(&consumers[i], NULL, consumer, (void *)i);
 
   pthread_exit(0);
+
   m_free(&mutex);
-  m_free(&empty);
-  m_free(&full);
+
   return(0);
 }
