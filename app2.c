0ca32ae5e5ae974de88b6ec99f6fd74ebe4c78da
Mateus Vendramini <mateusvendramini21@gmail.com>
2015-04-21 21:55:11 -0300

    porting app2 to pthreads semaphores, for debugging.

--- app2.c
+++ app2.c
@@ -9,11 +9,17 @@
 
 int rc;
 int wc;
-my_mutex mutex_rc;
+/*my_mutex mutex_rc;
 my_mutex mutex_wc;
 my_mutex mutex;
 my_mutex w_db;
-my_mutex r_db;
+my_mutex r_db;*/
+
+sem_t mutex_rc;
+sem_t mutex_wc;
+sem_t mutex;
+sem_t w_db;
+sem_t r_db;
 
 void do_work(int tid, char* thread_type, char* work) {
   printf("[%03d-%s] %s\n", tid, thread_type, work);
@@ -29,38 +35,46 @@ void *producer(void *arg){
   while (1){
 
     //down(mutex_wc);
-    m_lock(&mutex_wc, 1);
+    //m_lock(&mutex_wc, 1);
+    sem_wait(&mutex_wc);
 
     wc++;
 
     if (wc == 1){
       //down(r_db);
-      m_lock(&r_db, 1);
+      //m_lock(&r_db, 1);
+      sem_wait(&r_db);
     }
 
     //up(mutex_wc);
-    m_unlock(&mutex_rc, 1);
+    //m_unlock(&mutex_rc, 1);
+    sem_post(&mutex_rc);
 
     //down(w_db)
-    m_lock(&w_db, 1);
+    //m_lock(&w_db, 1);
+    sem_wait(&w_db);
 
     // TODO: escrita
     do_work(tid, "Produtor", "Escrevendo...");
 
     //up(w_db)
-    m_unlock(&w_db, 1);
+    //m_unlock(&w_db, 1);
+    sem_post(&w_db);
 
     //down(mutex_wc);
-    m_lock(&mutex_wc, 1);
+    //m_lock(&mutex_wc, 1);
+    sem_wait(&mutex_wc);
 
     wc--;
     if (wc == 0){
       //up(r_db);
-      m_unlock(&r_db, 1);
+      //m_unlock(&r_db, 1);
+      sem_post(&r_db);
     }
 
     //up(mutex_wc);
-    m_unlock(&mutex_wc, 1);
+    //m_unlock(&mutex_wc, 1);
+    sem_post(&mutex_wc);
 
   }
 
@@ -73,45 +87,55 @@ void *consumer(void *arg){
   while (1){
 
     //down(mutex);
-    m_lock(&mutex, 1);
+    //m_lock(&mutex, 1);
+    sem_wait(&mutex);
 
     //down(r_db);
-    m_lock(&r_db, 1);
+    //m_lock(&r_db, 1);
+    sem_wait(&r_db);
 
     //down(mutex_rc);
-    m_lock(&mutex_rc, 1);
+    //m_lock(&mutex_rc, 1);
+    sem_wait(&mutex_rc);
 
     rc++;
 
     if (rc == 1){
       //down(w_db);
-      m_lock(&w_db, 1);
+      //m_lock(&w_db, 1);
+      sem_wait(&w_db);
     }
 
     //up(mutex_rc);
-    m_unlock(&mutex_rc, 1);
+    //m_unlock(&mutex_rc, 1);
+    sem_post(&mutex_rc);
 
     //up(r_db);
-    m_unlock(&r_db, 1);
+    //m_unlock(&r_db, 1);
+    sem_post(&r_db);
 
     //up(mutex);
-    m_unlock(&mutex, 1);
+    //m_unlock(&mutex, 1);
+    sem_post(&mutex);
 
     // TODO: leitura
     do_work(tid, "Consumidor", "Lendo...");
 
     //down(mutex_rc);
-    m_lock(&mutex_rc, 1);
+    //m_lock(&mutex_rc, 1);
+    sem_wait(&mutex_rc);
 
     rc--;
 
     if (rc == 0){
       //up(w_db);
-      m_unlock(&w_db, 1);
+      //m_unlock(&w_db, 1);
+      sem_post(&w_db);
     }
 
     //up(mutex_rc);
-    m_unlock(&mutex_rc, 1);
+    //m_unlock(&mutex_rc, 1);
+    sem_post(&mutex_rc);
 
   }
 
@@ -125,11 +149,17 @@ int main(int argc, char *argv[]) {
   // INIT
   rc = 0;
   wc = 0;
-  m_init(&mutex_rc, 1);
+  /*m_init(&mutex_rc, 1);
   m_init(&mutex_wc, 1);
   m_init(&mutex, 1);
   m_init(&w_db, 1);
-  m_init(&r_db, 1);
+  m_init(&r_db, 1);*/
+
+  sem_init(&mutex_rc, 0, 1);
+  sem_init(&mutex_wc, 0, 1);
+  sem_init(&mutex, 0, 1);
+  sem_init(&w_db, 0, 1);
+  sem_init(&r_db, 0, 1);
 
   int i=0;
   for (i = 0; i < PRODUCERS; i++)
@@ -138,11 +168,11 @@ int main(int argc, char *argv[]) {
     pthread_create(&consumers[i], NULL, consumer, (void *)(intptr_t)i);
 
   pthread_exit(0);
-  m_free(&mutex_rc);
+  /*m_free(&mutex_rc);
   m_free(&mutex_wc);
   m_free(&mutex);
   m_free(&w_db);
-  m_free(&r_db);
+  m_free(&r_db);*/
 
   return 0;
 }
